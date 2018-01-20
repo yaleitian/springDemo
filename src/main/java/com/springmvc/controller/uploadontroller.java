9613486c82cfa28f1fb6c6a81658467983f2c2ef
lees <tianyaleimessage@gmail.com>
2018-01-20 21:44:06 +0800

    信息修改

---
+++ src/main/java/com/springmvc/controller/uploadontroller.java
@@ -0,0 +1,30 @@
+package com.springmvc.controller;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/uploadfile")
+public class uploadontroller {
+    @RequestMapping(value = "/upload",method = RequestMethod.GET)
+    public  String showUploadFilePage() {
+        return "/file";
+    }
+
+    @RequestMapping(value = "/beginUpload",method = RequestMethod.POST)
+    public String beginUpload(@RequestParam("file") MultipartFile file) throws IOException{
+        if (!file.isEmpty()) {
+            System.out.println("uploading file:{}" + file.getOriginalFilename());
+            FileUtils.copyInputStreamToFile(file.getInputStream(),new File( "/Users/lees/Downloads/work/ ",System.currentTimeMillis() + file.getOriginalFilename() ));
+        }
+         return "/successfile";
+    }
+}
