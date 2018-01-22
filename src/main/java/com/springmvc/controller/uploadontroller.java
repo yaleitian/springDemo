60c60d899dd884b0a512ed859c160ee2db21066a
lees <tianyaleimessage@gmail.com>
2018-01-22 21:54:39 +0800

    logger引入

--- src/main/java/com/springmvc/controller/uploadontroller.java
+++ src/main/java/com/springmvc/controller/uploadontroller.java
@@ -1,7 +1,7 @@
 package com.springmvc.controller;
 
 import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +14,7 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/uploadfile")
 public class uploadontroller {
+    static Logger logger = Logger.getLogger( uploadontroller.class );
     @RequestMapping(value = "/upload",method = RequestMethod.GET)
     public  String showUploadFilePage() {
         return "/file";
@@ -22,7 +23,8 @@ public class uploadontroller {
     @RequestMapping(value = "/beginUpload",method = RequestMethod.POST)
     public String beginUpload(@RequestParam("file") MultipartFile file) throws IOException{
         if (!file.isEmpty()) {
-            System.out.println("uploading file:{}" + file.getOriginalFilename());
+            //System.out.println("uploading file:{}" + file.getOriginalFilename());
+            logger.debug( "uploadling file : {}" + file.getOriginalFilename() );
             FileUtils.copyInputStreamToFile(file.getInputStream(),new File( "/Users/lees/Downloads/work/ ",System.currentTimeMillis() + file.getOriginalFilename() ));
         }
          return "/successfile";
